30b4c3902580fd90344930d438e2ebc0461169ec
ST4RFKR <nikelodiym1210@gmail.com>
2025-12-04 18:37:34 +0200

    fix: sync icon generation script with develop

--- assets/icons/components/CheckboxSelected.tsx
+++ assets/icons/components/CheckboxSelected.tsx
@@ -1,14 +1,21 @@
 import type { SVGProps } from 'react';
 import { Ref, forwardRef, memo } from 'react';
 const SvgCheckboxSelected = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width={18} height={18} fill='none' ref={ref} {...props}>
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={18}
+    height={18}
+    fill='none'
+    viewBox='0 0 18 18'
+    ref={ref}
+    {...props}
+  >
     <path
-      d='M16 0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.11 18 18 17.1 18 16V2C18 0.9 17.11 0 16 0ZM7 14L2 9L3.41 7.59L7 11.17L14.59 3.58L16 5L7 14Z'
-      fill='white'
+      fill='currentcolor'
+      d='M16 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M7 14 2 9l1.41-1.41L7 11.17l7.59-7.59L16 5z'
     />
   </svg>
 );
-
 const ForwardRef = forwardRef(SvgCheckboxSelected);
 const Memo = memo(ForwardRef);
 export default Memo;
