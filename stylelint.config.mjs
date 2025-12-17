83994928b041511fefbb572feeb38e2722ba8e21
AlexeiGitHub <0xaker0@gmail.com>
2025-12-17 17:55:40 +0200

    FRAM-37 fix: config stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,3 +1,13 @@
+/** @type {import('stylelint').Config} */
+
 export default {
   extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true,
+    'no-descending-specificity': null,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+    'keyframes-name-pattern': '^[a-z][a-zA-Z0-9]+$',
+    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+  },
 };
