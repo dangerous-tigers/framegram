e27edc658b96f8c27d23e85c2f3bd151aa700fe2
sergei00026 <sergei57fx@mail.ru>
2025-12-24 13:28:21 +0300

    FRAM-15-Pagination fead: add Select

--- app/(private)/feed/page.tsx
+++ app/(private)/feed/page.tsx
@@ -1,28 +1,3 @@
-'use client';
-import { Pagination } from '@/shared/ui/pagination/Pagination';
-import { useState } from 'react';
-
 export default function Page() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState('10');
-
-  const totalPages = 20; // приходит с бэка
-  return (
-    <div>
-      feed
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={(page) => {
-          if (page < 1 || page > totalPages) return;
-          setCurrentPage(page);
-        }}
-        pageSize={pageSize}
-        onPageSizeChange={(value) => {
-          setPageSize(value);
-          setCurrentPage(1); // ⚠️ важно
-        }}
-      />
-    </div>
-  );
+  return <div>feed</div>;
 }
