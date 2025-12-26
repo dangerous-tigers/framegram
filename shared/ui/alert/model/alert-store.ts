70a6c802cd5195f5c02982e3665cc160b98734b7
sergei00026 <sergei57fx@mail.ru>
2025-12-26 16:22:48 +0300

    FRAM-26-Alert fix: styles

--- shared/ui/alert/model/alert-store.ts
+++ shared/ui/alert/model/alert-store.ts
@@ -5,7 +5,7 @@ type Severity = 'error' | 'success';
 type Variant = 'default' | 'filled' | 'outlined';
 
 const initialState = {
-  open: false,
+  open: true,
   error: null as Nullable<string>,
   description: null as Nullable<string>,
   variant: 'default' as Variant,
