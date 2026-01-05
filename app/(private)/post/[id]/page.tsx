5d84927ea173f1654615900073ca91f69f66243a
ST4RFKR <nikelodiym1210@gmail.com>
2026-01-05 18:44:32 +0200

    feat: redirect to 404 when post not found

--- app/(private)/post/[id]/page.tsx
+++ app/(private)/post/[id]/page.tsx
@@ -1,18 +1,21 @@
 import { postApi } from '@/entities/post/api/post.api';
 import { PostViewModal } from '@/features/post/viewPost';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-
-  const post = await postApi.getPostByIdServer(+id);
-
-  return (
-    <div>
-      <PostViewModal
-        open
-        defaultOpen
-        post={post}
-      />
-    </div>
-  );
+  try {
+    const { id } = await params;
+    const post = await postApi.getPostByIdServer(+id);
+    return (
+      <div>
+        <PostViewModal
+          open
+          defaultOpen
+          post={post}
+        />
+      </div>
+    );
+  } catch {
+    notFound();
+  }
 }
