76a662823a6fc181eb8901814d5e17eca333e217
hrsk <hrsk.yegor@gmail.com>
2026-01-16 19:04:21 +0300

    FRAM-55-About feat: posts request, component on posts

--- app/(public)/profile/[id]/page.tsx
+++ app/(public)/profile/[id]/page.tsx
@@ -1,10 +1,15 @@
-import { Profile } from '@/features/profile/ui/Profile';
+import { PostsByUserId, PublicProfileViewModelResponse } from '@/entities/profile';
+import { Profile } from '@/features/profile';
+
 export default async function ProfilePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
-  const getProfileByUserId = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/public-user/profile/${id}`).then((res) =>
-    res.json(),
-  );
+  const [getPostByUser, getProfileByUserId] = await Promise.all<
+    [Promise<PostsByUserId>, Promise<PublicProfileViewModelResponse>]
+  >([
+    fetch(`${process.env.NEXT_PUBLIC_BASEURL}/posts/user/${id}/?pageSize=${12}`).then((res) => res.json()),
+    fetch(`${process.env.NEXT_PUBLIC_BASEURL}/public-user/profile/${id}`).then((res) => res.json()),
+  ]);
 
   const profileByUserName = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/users/${getProfileByUserId.userName}`).then(
     (res) => res.json(),
@@ -12,6 +17,7 @@ export default async function ProfilePage({ params }: { params: Promise<{ id: st
 
   return (
     <Profile
+      items={getPostByUser.items}
       profile={profileByUserName}
       hasPaymentSubscription={getProfileByUserId.hasPaymentSubscription}
     />
--- features/profile/ui/Profile.tsx
+++ features/profile/ui/Profile.tsx
@@ -1,20 +1,22 @@
 import s from './Profile.module.scss';
 
-import { UserProfileByIdWithPostsResponse } from '@/entities/profile';
-import { ProfileHeader } from '@/features/profile/profile-header/ui/ProfileHeader';
+import { PostViewModel, UserProfileByIdWithPostsResponse } from '@/entities/profile';
+import { Posts, ProfileHeader } from '@/features/profile';
 
 type Props = {
   profile: UserProfileByIdWithPostsResponse;
+  items: PostViewModel[];
   hasPaymentSubscription: boolean;
 };
 
-export const Profile = ({ profile, hasPaymentSubscription }: Props) => {
+export const Profile = ({ profile, items, hasPaymentSubscription }: Props) => {
   return (
     <div className={s.container}>
       <ProfileHeader
         profile={profile}
         hasPaymentSubscription={hasPaymentSubscription}
       />
+      <Posts items={items} />
     </div>
   );
 };
