a315945099660e9c07741e9c6d4b3a51707d2cb4
hrsk <hrsk.yegor@gmail.com>
2026-01-27 12:46:44 +0300

    fix: swiper buttons

--- shared/ui/swiper/Swiper.tsx
+++ shared/ui/swiper/Swiper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import clsx from 'clsx';
-import { useRef } from 'react';
 import type { Swiper as SwiperInstance } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -12,20 +11,14 @@ import { Swiper as SwiperRoot, SwiperSlide } from 'swiper/react';
 import s from './swiper.module.scss';
 import type { AppSwiperProps } from './types';
 
-import { ArrowIosBackOutline, ArrowIosForwardOutline } from '@/assets/icons';
-
 export const Swiper = ({
   slides,
   className,
   options,
-  withNavigation = true,
-  withPagination = true,
   initialIndex = 0,
   onIndexChange,
   rootClassName,
 }: AppSwiperProps) => {
-  const prevRef = useRef<HTMLButtonElement | null>(null);
-  const nextRef = useRef<HTMLButtonElement | null>(null);
   const hasManySlides = slides.length > 1;
 
   const handleSlideChange = (swiper: SwiperInstance) => {
@@ -34,28 +27,6 @@ export const Swiper = ({
 
   return (
     <div className={clsx(s.root, className)}>
-      {withNavigation && (
-        <>
-          <button
-            ref={prevRef}
-            type='button'
-            className={clsx('swiper-button-prev', s.prev)}
-            aria-label='Previous slide'
-          >
-            <ArrowIosBackOutline />
-          </button>
-
-          <button
-            ref={nextRef}
-            type='button'
-            className={clsx('swiper-button-next', s.next)}
-            aria-label='Next slide'
-          >
-            <ArrowIosForwardOutline />
-          </button>
-        </>
-      )}
-
       <SwiperRoot
         className={rootClassName}
         modules={[Navigation, Pagination, A11y, Keyboard]}
@@ -66,21 +37,8 @@ export const Swiper = ({
           prevSlideMessage: 'Previous slide',
           nextSlideMessage: 'Next slide',
         }}
-        navigation={
-          withNavigation
-            ? {
-                prevEl: prevRef.current,
-                nextEl: nextRef.current,
-              }
-            : false
-        }
-        pagination={withPagination && hasManySlides ? { clickable: true } : false}
-        onBeforeInit={(swiper) => {
-          if (typeof swiper.params.navigation !== 'boolean') {
-            swiper.params.navigation!.prevEl = prevRef.current;
-            swiper.params.navigation!.nextEl = nextRef.current;
-          }
-        }}
+        navigation={hasManySlides}
+        pagination={hasManySlides ? { clickable: true } : false}
         initialSlide={initialIndex}
         onSlideChange={handleSlideChange}
         {...options}
