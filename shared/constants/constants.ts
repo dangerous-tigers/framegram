746af21e9b703799dfce25add02952eb32c1c09f
Anatoliy Manzhola <sirius_star@inbox.ru>
2026-02-04 22:41:51 +0300

    init comit

--- shared/constants/constants.ts
+++ shared/constants/constants.ts
@@ -2,5 +2,5 @@ export const ACCESS_TOKEN = 'access-token';
 
 export const TEXT_AREA_MAX_LENGTH = 500;
 
-export const PORTION_POSTS = 4;
+export const PORTION_POSTS = 12;
 export const PORTION_COMMENTS = 4;
